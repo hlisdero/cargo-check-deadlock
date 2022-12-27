98fc63d25d1550f812ff4768323a7f95371ff011
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-12-27 18:37:03 +0100

    Delete example translation. It is no longer needed.

--- src/translator.rs
+++ src/translator.rs
@@ -146,29 +146,4 @@ impl<'tcx> Translator<'tcx> {
             self.call_stack.pop();
         }
     }
-
-    /// Example translation
-    /// <https://rustc-dev-guide.rust-lang.org/rustc-driver-interacting-with-the-ast.html>
-    /// Iterate over the top-level items in the crate, looking for the main function.
-    pub fn print_all_expr_hir(&self) {
-        // Every compilation contains a single crate.
-        let hir_krate = self.tcx.hir();
-        for id in hir_krate.items() {
-            let item = hir_krate.item(id);
-            // Use pattern-matching to find a specific node inside the main function.
-            if let rustc_hir::ItemKind::Fn(_, _, body_id) = item.kind {
-                let expr = self.tcx.hir().body(body_id).value;
-                if let rustc_hir::ExprKind::Block(block, _) = expr.kind {
-                    if let rustc_hir::StmtKind::Local(local) = block.stmts[0].kind {
-                        if let Some(expr) = local.init {
-                            let hir_id = expr.hir_id; // hir_id identifies the string "Hello, world!"
-                            let def_id = self.tcx.hir().local_def_id(item.hir_id()); // def_id identifies the main function
-                            let ty = self.tcx.typeck(def_id).node_type(hir_id);
-                            println!("{expr:#?}: {ty:?}");
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
