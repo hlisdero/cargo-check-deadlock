a83dfbe663e971441e2542330512ef0443fe1f4a
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-12-27 12:25:09 +0100

    Add first implementation of `Mutex` and `MutexManager`

---
+++ src/translator/mutex.rs
@@ -0,0 +1,46 @@
+//! Representation of a mutex in the Petri net.
+//!
+//! The `Mutex` stores one reference to the place in the Petri net
+//! that models the state of the mutex.
+//!
+//! If the place has a token, the mutex is unlocked.
+//! If the place does not have a token, the mutex is locked.
+use crate::translator::naming::mutex_place_label_from_index;
+use netcrab::petri_net::{PetriNet, PlaceRef, TransitionRef};
+
+pub struct Mutex {
+    place_ref: PlaceRef,
+}
+
+impl Mutex {
+    /// Creates a new `Mutex` whose label is based on `index`.
+    /// Adds a `Place` to the Petri Net.
+    pub fn new(index: usize, net: &mut PetriNet) -> Self {
+        let label = mutex_place_label_from_index(index);
+        let place_ref = net.add_place(&label);
+        Self { place_ref }
+    }
+
+    /// Returns `true` if the `Mutex` is locked
+    ///
+    /// # Panics
+    ///
+    /// If the stored place reference is invalid for the given Petri net, the function panics.
+    /// If the place contains a number of tokens that is not 1 or 0, the function panics.
+    pub fn is_locked(&self, net: &PetriNet) -> bool {
+        match net.marking(&self.place_ref) {
+            Ok(1) => true,
+            Ok(0) => false,
+            Ok(_) => panic!("BUG: The mutex's place may only have 1 or 0 tokens"),
+            Err(_) => panic!("BUG: The mutex's place reference should be valid for the net"),
+        }
+    }
+
+    /// Adds a lock guard for this `Mutex`.
+    /// Connects the mutex's place to the transition, then the transition will only
+    /// fire if the mutex is unlocked.
+    pub fn add_lock_guard(&self, transition_lock: &TransitionRef, net: &mut PetriNet) {
+        net.add_arc_place_transition(&self.place_ref, transition_lock)
+            .expect("BUG: Adding an arc from the mutex's place to the lock guard should not fail");
+    }
+}
---
+++ src/translator/mutex_manager.rs
@@ -0,0 +1,106 @@
+//! Auxiliary structure to keep track of the mutexes in the code.
+//!
+//! The `MutexManager` stores the mutexes discovered so far in the code.
+//! It also stores the lock guards, i.e. the local variables that result
+//! from a call to `std::sync::Mutex::lock()`.
+//! It also stores the links, i.e. the local variables that reference a mutex.
+//! If a mutex A is accessed in two threads then two variables
+//! (the original and the `.clone()` of the `std::sync::Arc`) is a link to mutex A.
+use crate::translator::local::Local;
+use crate::translator::mutex::Mutex;
+use netcrab::petri_net::{PetriNet, TransitionRef};
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct MutexManager {
+    mutexes: Vec<Mutex>,
+    links: HashMap<Local, MutexRef>,
+    guards: HashMap<Local, MutexRef>,
+}
+
+/// A wrapper type around the indexes to the elements in `Vec<Mutex>`.
+#[derive(Clone)]
+struct MutexRef(usize);
+
+impl MutexManager {
+    /// Return a new empty `MutexManager`
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Return the `Mutex` associated to the mutex reference.
+    ///
+    /// # Errors
+    ///
+    /// If the `MutexRef` is invalid, then an error is returned.
+    fn get_mutex_from_ref(&self, mutex_ref: &MutexRef) -> Result<&Mutex, &str> {
+        if mutex_ref.0 >= self.mutexes.len() {
+            return Err("The given mutex reference is invalid");
+        }
+        Ok(&self.mutexes[mutex_ref.0])
+    }
+
+    /// Return the `Mutex` contained in the `Local` variable.
+    ///
+    /// # Errors
+    ///
+    /// If the `Local` is not associated to a `Mutex`, then an error is returned.
+    pub fn get_mutex_from_local(&self, local: &Local) -> Result<&Mutex, &str> {
+        let Some(mutex_ref) = self.links.get(local) else {
+            return Err("The local variable is not associated to a mutex")
+        };
+        self.get_mutex_from_ref(mutex_ref)
+    }
+
+    /// Add a mutex and create its corresponding representation in the Petri net.
+    pub fn add_mutex(&mut self, net: &mut PetriNet) -> MutexRef {
+        let index = self.mutexes.len();
+        self.mutexes.push(Mutex::new(index, net));
+        MutexRef(index)
+    }
+
+    /// Adds a lock guard to the mutex.
+    /// Connects the mutex's place to the transition, then the transition will only
+    /// fire if the mutex is unlocked.
+    ///
+    /// # Errors
+    ///
+    /// If the `Local` is not associated to a `Mutex`, then an error is returned.
+    pub fn add_lock_guard(
+        &mut self,
+        guard: Local,
+        local_with_mutex: Local,
+        transition_lock: TransitionRef,
+        net: &mut PetriNet,
+    ) -> Result<(), &str> {
+        let Some(mutex_ref) = self.links.get(&local_with_mutex) else {
+            return Err("The local variable is not associated to a mutex");
+        };
+        self.guards.insert(guard, mutex_ref.clone());
+
+        let mutex = match self.get_mutex_from_ref(mutex_ref) {
+            Ok(mutex) => mutex,
+            Err(err_str) => return Err(err_str),
+        };
+        mutex.add_lock_guard(&transition_lock, net);
+        Ok(())
+    }
+
+    /// Check if the `Local` variable is linked to a `Mutex`.
+    /// Return true if the variable contains a mutex.
+    pub fn is_linked_to_mutex(&self, local: &Local) -> bool {
+        self.links.contains_key(local)
+    }
+
+    /// Link the local variable to a specific mutex through its reference.
+    ///
+    /// If the local variable did not have an associated mutex, `None` is returned.
+    /// If the local variable had an associated mutex, the value is updated and the old value is returned.
+    ///
+    /// This could overwrite a previous link to a different mutex.
+    /// It does not occur often that one "reuses" a variable for two different mutexes
+    /// but it is allowed in theory.
+    pub fn link(&mut self, local: Local, mutex_ref: MutexRef) -> Option<MutexRef> {
+        self.links.insert(local, mutex_ref)
+    }
+}
