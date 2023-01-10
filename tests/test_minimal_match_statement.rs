881a3873a3b060e70bc727f670f9426d14327fa2
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-01-10 22:39:49 +0100

    Fix tests for minimal match statement: The compiler internals changed.
    This is the first time that this happens. The resulting MIR translation varied significantly. We can only check manually again and verify that the translation is correct.

--- tests/test_minimal_match_statement.rs
+++ tests/test_minimal_match_statement.rs
@@ -10,51 +10,29 @@ const MINIMAL_MATCH_STATEMENT_DOT_OUTPUT: &str = r#"digraph petrinet {
     main_BASIC_BLOCK_3 [shape="circle" xlabel="main_BASIC_BLOCK_3" label=""];
     main_BASIC_BLOCK_4 [shape="circle" xlabel="main_BASIC_BLOCK_4" label=""];
     main_BASIC_BLOCK_5 [shape="circle" xlabel="main_BASIC_BLOCK_5" label=""];
-    main_BASIC_BLOCK_6 [shape="circle" xlabel="main_BASIC_BLOCK_6" label=""];
-    main_BASIC_BLOCK_7 [shape="circle" xlabel="main_BASIC_BLOCK_7" label=""];
-    main_BASIC_BLOCK_8 [shape="circle" xlabel="main_BASIC_BLOCK_8" label=""];
-    main_BASIC_BLOCK_9 [shape="circle" xlabel="main_BASIC_BLOCK_9" label=""];
     main_BASIC_BLOCK_END_PLACE_1 [shape="circle" xlabel="main_BASIC_BLOCK_END_PLACE_1" label=""];
     main_BLOCK_1_STATEMENT_0 [shape="box" xlabel="main_BLOCK_1_STATEMENT_0" label=""];
-    main_DROP_6 [shape="box" xlabel="main_DROP_6" label=""];
-    main_DROP_8 [shape="box" xlabel="main_DROP_8" label=""];
-    main_GOTO_3 [shape="box" xlabel="main_GOTO_3" label=""];
-    main_GOTO_4 [shape="box" xlabel="main_GOTO_4" label=""];
-    main_GOTO_5 [shape="box" xlabel="main_GOTO_5" label=""];
+    main_DROP_2 [shape="box" xlabel="main_DROP_2" label=""];
+    main_DROP_4 [shape="box" xlabel="main_DROP_4" label=""];
     main_RETURN [shape="box" xlabel="main_RETURN" label=""];
-    main_SWITCH_INT_3 [shape="box" xlabel="main_SWITCH_INT_3" label=""];
-    main_SWITCH_INT_4 [shape="box" xlabel="main_SWITCH_INT_4" label=""];
-    main_SWITCH_INT_5 [shape="box" xlabel="main_SWITCH_INT_5" label=""];
-    main_UNWIND_9 [shape="box" xlabel="main_UNWIND_9" label=""];
+    main_UNWIND_5 [shape="box" xlabel="main_UNWIND_5" label=""];
     std_env_args_FOREIGN_CALL [shape="box" xlabel="std_env_args_FOREIGN_CALL" label=""];
     std_iter_ExactSizeIterator_len_FOREIGN_CALL [shape="box" xlabel="std_iter_ExactSizeIterator_len_FOREIGN_CALL" label=""];
     PROGRAM_START -> std_env_args_FOREIGN_CALL;
     main_BASIC_BLOCK_1 -> main_BLOCK_1_STATEMENT_0;
-    main_BASIC_BLOCK_2 -> main_SWITCH_INT_3;
-    main_BASIC_BLOCK_2 -> main_SWITCH_INT_4;
-    main_BASIC_BLOCK_2 -> main_SWITCH_INT_5;
-    main_BASIC_BLOCK_3 -> main_GOTO_3;
-    main_BASIC_BLOCK_4 -> main_GOTO_4;
-    main_BASIC_BLOCK_5 -> main_GOTO_5;
-    main_BASIC_BLOCK_6 -> main_DROP_6;
-    main_BASIC_BLOCK_7 -> main_RETURN;
-    main_BASIC_BLOCK_8 -> main_DROP_8;
-    main_BASIC_BLOCK_9 -> main_UNWIND_9;
+    main_BASIC_BLOCK_2 -> main_DROP_2;
+    main_BASIC_BLOCK_3 -> main_RETURN;
+    main_BASIC_BLOCK_4 -> main_DROP_4;
+    main_BASIC_BLOCK_5 -> main_UNWIND_5;
     main_BASIC_BLOCK_END_PLACE_1 -> std_iter_ExactSizeIterator_len_FOREIGN_CALL;
     main_BLOCK_1_STATEMENT_0 -> main_BASIC_BLOCK_END_PLACE_1;
-    main_DROP_6 -> main_BASIC_BLOCK_7;
-    main_DROP_8 -> main_BASIC_BLOCK_9;
-    main_GOTO_3 -> main_BASIC_BLOCK_6;
-    main_GOTO_4 -> main_BASIC_BLOCK_6;
-    main_GOTO_5 -> main_BASIC_BLOCK_6;
+    main_DROP_2 -> main_BASIC_BLOCK_3;
+    main_DROP_4 -> main_BASIC_BLOCK_5;
     main_RETURN -> PROGRAM_END;
-    main_SWITCH_INT_3 -> main_BASIC_BLOCK_3;
-    main_SWITCH_INT_4 -> main_BASIC_BLOCK_4;
-    main_SWITCH_INT_5 -> main_BASIC_BLOCK_5;
-    main_UNWIND_9 -> PROGRAM_PANIC;
+    main_UNWIND_5 -> PROGRAM_PANIC;
     std_env_args_FOREIGN_CALL -> main_BASIC_BLOCK_1;
     std_iter_ExactSizeIterator_len_FOREIGN_CALL -> main_BASIC_BLOCK_2;
-    std_iter_ExactSizeIterator_len_FOREIGN_CALL -> main_BASIC_BLOCK_8;
+    std_iter_ExactSizeIterator_len_FOREIGN_CALL -> main_BASIC_BLOCK_4;
 }
 "#;
 
@@ -67,10 +45,6 @@ const MINIMAL_MATCH_STATEMENT_LOLA_OUTPUT: &str = r#"PLACE
     main_BASIC_BLOCK_3,
     main_BASIC_BLOCK_4,
     main_BASIC_BLOCK_5,
-    main_BASIC_BLOCK_6,
-    main_BASIC_BLOCK_7,
-    main_BASIC_BLOCK_8,
-    main_BASIC_BLOCK_9,
     main_BASIC_BLOCK_END_PLACE_1;
 
 MARKING
@@ -80,54 +54,24 @@ TRANSITION main_BLOCK_1_STATEMENT_0
     main_BASIC_BLOCK_1 : 1;
   PRODUCE
     main_BASIC_BLOCK_END_PLACE_1 : 1;
-TRANSITION main_DROP_6
+TRANSITION main_DROP_2
   CONSUME
-    main_BASIC_BLOCK_6 : 1;
+    main_BASIC_BLOCK_2 : 1;
   PRODUCE
-    main_BASIC_BLOCK_7 : 1;
-TRANSITION main_DROP_8
-  CONSUME
-    main_BASIC_BLOCK_8 : 1;
-  PRODUCE
-    main_BASIC_BLOCK_9 : 1;
-TRANSITION main_GOTO_3
-  CONSUME
     main_BASIC_BLOCK_3 : 1;
-  PRODUCE
-    main_BASIC_BLOCK_6 : 1;
-TRANSITION main_GOTO_4
+TRANSITION main_DROP_4
   CONSUME
     main_BASIC_BLOCK_4 : 1;
   PRODUCE
-    main_BASIC_BLOCK_6 : 1;
-TRANSITION main_GOTO_5
-  CONSUME
     main_BASIC_BLOCK_5 : 1;
-  PRODUCE
-    main_BASIC_BLOCK_6 : 1;
 TRANSITION main_RETURN
   CONSUME
-    main_BASIC_BLOCK_7 : 1;
+    main_BASIC_BLOCK_3 : 1;
   PRODUCE
     PROGRAM_END : 1;
-TRANSITION main_SWITCH_INT_3
+TRANSITION main_UNWIND_5
   CONSUME
-    main_BASIC_BLOCK_2 : 1;
-  PRODUCE
-    main_BASIC_BLOCK_3 : 1;
-TRANSITION main_SWITCH_INT_4
-  CONSUME
-    main_BASIC_BLOCK_2 : 1;
-  PRODUCE
-    main_BASIC_BLOCK_4 : 1;
-TRANSITION main_SWITCH_INT_5
-  CONSUME
-    main_BASIC_BLOCK_2 : 1;
-  PRODUCE
     main_BASIC_BLOCK_5 : 1;
-TRANSITION main_UNWIND_9
-  CONSUME
-    main_BASIC_BLOCK_9 : 1;
   PRODUCE
     PROGRAM_PANIC : 1;
 TRANSITION std_env_args_FOREIGN_CALL
@@ -140,7 +84,7 @@ TRANSITION std_iter_ExactSizeIterator_len_FOREIGN_CALL
     main_BASIC_BLOCK_END_PLACE_1 : 1;
   PRODUCE
     main_BASIC_BLOCK_2 : 1,
-    main_BASIC_BLOCK_8 : 1;
+    main_BASIC_BLOCK_4 : 1;
 "#;
 
 const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encoding="utf-8"?>
@@ -190,26 +134,6 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>main_BASIC_BLOCK_5</text>
         </name>
       </place>
-      <place id="main_BASIC_BLOCK_6">
-        <name>
-          <text>main_BASIC_BLOCK_6</text>
-        </name>
-      </place>
-      <place id="main_BASIC_BLOCK_7">
-        <name>
-          <text>main_BASIC_BLOCK_7</text>
-        </name>
-      </place>
-      <place id="main_BASIC_BLOCK_8">
-        <name>
-          <text>main_BASIC_BLOCK_8</text>
-        </name>
-      </place>
-      <place id="main_BASIC_BLOCK_9">
-        <name>
-          <text>main_BASIC_BLOCK_9</text>
-        </name>
-      </place>
       <place id="main_BASIC_BLOCK_END_PLACE_1">
         <name>
           <text>main_BASIC_BLOCK_END_PLACE_1</text>
@@ -220,29 +144,14 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>main_BLOCK_1_STATEMENT_0</text>
         </name>
       </transition>
-      <transition id="main_DROP_6">
+      <transition id="main_DROP_2">
         <name>
-          <text>main_DROP_6</text>
+          <text>main_DROP_2</text>
         </name>
       </transition>
-      <transition id="main_DROP_8">
+      <transition id="main_DROP_4">
         <name>
-          <text>main_DROP_8</text>
-        </name>
-      </transition>
-      <transition id="main_GOTO_3">
-        <name>
-          <text>main_GOTO_3</text>
-        </name>
-      </transition>
-      <transition id="main_GOTO_4">
-        <name>
-          <text>main_GOTO_4</text>
-        </name>
-      </transition>
-      <transition id="main_GOTO_5">
-        <name>
-          <text>main_GOTO_5</text>
+          <text>main_DROP_4</text>
         </name>
       </transition>
       <transition id="main_RETURN">
@@ -250,24 +159,9 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>main_RETURN</text>
         </name>
       </transition>
-      <transition id="main_SWITCH_INT_3">
+      <transition id="main_UNWIND_5">
         <name>
-          <text>main_SWITCH_INT_3</text>
-        </name>
-      </transition>
-      <transition id="main_SWITCH_INT_4">
-        <name>
-          <text>main_SWITCH_INT_4</text>
-        </name>
-      </transition>
-      <transition id="main_SWITCH_INT_5">
-        <name>
-          <text>main_SWITCH_INT_5</text>
-        </name>
-      </transition>
-      <transition id="main_UNWIND_9">
-        <name>
-          <text>main_UNWIND_9</text>
+          <text>main_UNWIND_5</text>
         </name>
       </transition>
       <transition id="std_env_args_FOREIGN_CALL">
@@ -296,81 +190,33 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_BASIC_BLOCK_2" target="main_SWITCH_INT_3" id="(main_BASIC_BLOCK_2, main_SWITCH_INT_3)">
+      <arc source="main_BASIC_BLOCK_2" target="main_DROP_2" id="(main_BASIC_BLOCK_2, main_DROP_2)">
         <name>
-          <text>(main_BASIC_BLOCK_2, main_SWITCH_INT_3)</text>
+          <text>(main_BASIC_BLOCK_2, main_DROP_2)</text>
         </name>
         <inscription>
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_BASIC_BLOCK_2" target="main_SWITCH_INT_4" id="(main_BASIC_BLOCK_2, main_SWITCH_INT_4)">
+      <arc source="main_BASIC_BLOCK_3" target="main_RETURN" id="(main_BASIC_BLOCK_3, main_RETURN)">
         <name>
-          <text>(main_BASIC_BLOCK_2, main_SWITCH_INT_4)</text>
+          <text>(main_BASIC_BLOCK_3, main_RETURN)</text>
         </name>
         <inscription>
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_BASIC_BLOCK_2" target="main_SWITCH_INT_5" id="(main_BASIC_BLOCK_2, main_SWITCH_INT_5)">
+      <arc source="main_BASIC_BLOCK_4" target="main_DROP_4" id="(main_BASIC_BLOCK_4, main_DROP_4)">
         <name>
-          <text>(main_BASIC_BLOCK_2, main_SWITCH_INT_5)</text>
+          <text>(main_BASIC_BLOCK_4, main_DROP_4)</text>
         </name>
         <inscription>
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_BASIC_BLOCK_3" target="main_GOTO_3" id="(main_BASIC_BLOCK_3, main_GOTO_3)">
+      <arc source="main_BASIC_BLOCK_5" target="main_UNWIND_5" id="(main_BASIC_BLOCK_5, main_UNWIND_5)">
         <name>
-          <text>(main_BASIC_BLOCK_3, main_GOTO_3)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_BASIC_BLOCK_4" target="main_GOTO_4" id="(main_BASIC_BLOCK_4, main_GOTO_4)">
-        <name>
-          <text>(main_BASIC_BLOCK_4, main_GOTO_4)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_BASIC_BLOCK_5" target="main_GOTO_5" id="(main_BASIC_BLOCK_5, main_GOTO_5)">
-        <name>
-          <text>(main_BASIC_BLOCK_5, main_GOTO_5)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_BASIC_BLOCK_6" target="main_DROP_6" id="(main_BASIC_BLOCK_6, main_DROP_6)">
-        <name>
-          <text>(main_BASIC_BLOCK_6, main_DROP_6)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_BASIC_BLOCK_7" target="main_RETURN" id="(main_BASIC_BLOCK_7, main_RETURN)">
-        <name>
-          <text>(main_BASIC_BLOCK_7, main_RETURN)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_BASIC_BLOCK_8" target="main_DROP_8" id="(main_BASIC_BLOCK_8, main_DROP_8)">
-        <name>
-          <text>(main_BASIC_BLOCK_8, main_DROP_8)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_BASIC_BLOCK_9" target="main_UNWIND_9" id="(main_BASIC_BLOCK_9, main_UNWIND_9)">
-        <name>
-          <text>(main_BASIC_BLOCK_9, main_UNWIND_9)</text>
+          <text>(main_BASIC_BLOCK_5, main_UNWIND_5)</text>
         </name>
         <inscription>
           <text>1</text>
@@ -392,41 +238,17 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_DROP_6" target="main_BASIC_BLOCK_7" id="(main_DROP_6, main_BASIC_BLOCK_7)">
+      <arc source="main_DROP_2" target="main_BASIC_BLOCK_3" id="(main_DROP_2, main_BASIC_BLOCK_3)">
         <name>
-          <text>(main_DROP_6, main_BASIC_BLOCK_7)</text>
+          <text>(main_DROP_2, main_BASIC_BLOCK_3)</text>
         </name>
         <inscription>
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_DROP_8" target="main_BASIC_BLOCK_9" id="(main_DROP_8, main_BASIC_BLOCK_9)">
+      <arc source="main_DROP_4" target="main_BASIC_BLOCK_5" id="(main_DROP_4, main_BASIC_BLOCK_5)">
         <name>
-          <text>(main_DROP_8, main_BASIC_BLOCK_9)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_GOTO_3" target="main_BASIC_BLOCK_6" id="(main_GOTO_3, main_BASIC_BLOCK_6)">
-        <name>
-          <text>(main_GOTO_3, main_BASIC_BLOCK_6)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_GOTO_4" target="main_BASIC_BLOCK_6" id="(main_GOTO_4, main_BASIC_BLOCK_6)">
-        <name>
-          <text>(main_GOTO_4, main_BASIC_BLOCK_6)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_GOTO_5" target="main_BASIC_BLOCK_6" id="(main_GOTO_5, main_BASIC_BLOCK_6)">
-        <name>
-          <text>(main_GOTO_5, main_BASIC_BLOCK_6)</text>
+          <text>(main_DROP_4, main_BASIC_BLOCK_5)</text>
         </name>
         <inscription>
           <text>1</text>
@@ -440,33 +262,9 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="main_SWITCH_INT_3" target="main_BASIC_BLOCK_3" id="(main_SWITCH_INT_3, main_BASIC_BLOCK_3)">
+      <arc source="main_UNWIND_5" target="PROGRAM_PANIC" id="(main_UNWIND_5, PROGRAM_PANIC)">
         <name>
-          <text>(main_SWITCH_INT_3, main_BASIC_BLOCK_3)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_SWITCH_INT_4" target="main_BASIC_BLOCK_4" id="(main_SWITCH_INT_4, main_BASIC_BLOCK_4)">
-        <name>
-          <text>(main_SWITCH_INT_4, main_BASIC_BLOCK_4)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_SWITCH_INT_5" target="main_BASIC_BLOCK_5" id="(main_SWITCH_INT_5, main_BASIC_BLOCK_5)">
-        <name>
-          <text>(main_SWITCH_INT_5, main_BASIC_BLOCK_5)</text>
-        </name>
-        <inscription>
-          <text>1</text>
-        </inscription>
-      </arc>
-      <arc source="main_UNWIND_9" target="PROGRAM_PANIC" id="(main_UNWIND_9, PROGRAM_PANIC)">
-        <name>
-          <text>(main_UNWIND_9, PROGRAM_PANIC)</text>
+          <text>(main_UNWIND_5, PROGRAM_PANIC)</text>
         </name>
         <inscription>
           <text>1</text>
@@ -488,9 +286,9 @@ const MINIMAL_MATCH_STATEMENT_PNML_OUTPUT: &str = r#"<?xml version="1.0" encodin
           <text>1</text>
         </inscription>
       </arc>
-      <arc source="std_iter_ExactSizeIterator_len_FOREIGN_CALL" target="main_BASIC_BLOCK_8" id="(std_iter_ExactSizeIterator_len_FOREIGN_CALL, main_BASIC_BLOCK_8)">
+      <arc source="std_iter_ExactSizeIterator_len_FOREIGN_CALL" target="main_BASIC_BLOCK_4" id="(std_iter_ExactSizeIterator_len_FOREIGN_CALL, main_BASIC_BLOCK_4)">
         <name>
-          <text>(std_iter_ExactSizeIterator_len_FOREIGN_CALL, main_BASIC_BLOCK_8)</text>
+          <text>(std_iter_ExactSizeIterator_len_FOREIGN_CALL, main_BASIC_BLOCK_4)</text>
         </name>
         <inscription>
           <text>1</text>
