6fb53254be44d6a7c3ba862e0cf00ca97c28c59d
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-02-05 14:24:27 +0100

    Extract the common logic in handling assignments to a common reusable function

--- src/translator/sync.rs
+++ src/translator/sync.rs
@@ -60,37 +60,46 @@ pub fn handle_use_move_assignment<'tcx>(
     caller_function_def_id: rustc_hir::def_id::DefId,
     tcx: rustc_middle::ty::TyCtxt<'tcx>,
 ) {
-    if is_place_with_concrete_type(rhs, "std::sync::Mutex<T>", caller_function_def_id, tcx)
-        || is_place_with_concrete_type(
-            rhs,
-            "std::sync::Arc<std::sync::Mutex<T>>",
-            caller_function_def_id,
-            tcx,
-        )
-    {
-        memory.link_place_to_same_mutex(*place, *rhs);
-    } else if is_place_with_concrete_type(
-        rhs,
-        "std::sync::MutexGuard<'a, T>",
-        caller_function_def_id,
-        tcx,
-    ) {
-        memory.link_place_to_same_lock_guard(*place, *rhs);
-    } else if is_place_with_concrete_type(
-        rhs,
-        "std::thread::JoinHandle<T>",
-        caller_function_def_id,
-        tcx,
-    ) {
-        memory.link_place_to_same_join_handle(*place, *rhs);
-    } else if is_place_with_concrete_type(rhs, "std::sync::Condvar", caller_function_def_id, tcx) {
-        memory.link_place_to_same_condvar(*place, *rhs);
-    }
+    handle_assignment_sync_variable(place, rhs, memory, caller_function_def_id, tcx);
 }
 
 /// Handles MIR assignments of the form: `_X = &_Y`.
 /// If the right-hand side contains a synchronization variable, link it to the left-hand side.
 pub fn handle_ref_assignment<'tcx>(
+    place: &rustc_middle::mir::Place<'tcx>,
+    rhs: &rustc_middle::mir::Place<'tcx>,
+    memory: &mut Memory<'tcx>,
+    caller_function_def_id: rustc_hir::def_id::DefId,
+    tcx: rustc_middle::ty::TyCtxt<'tcx>,
+) {
+    handle_assignment_sync_variable(place, rhs, memory, caller_function_def_id, tcx);
+}
+
+/// Handles MIR assignments of the form: `_X = { copy_data: move _Y }`.
+/// If the right-hand side contains a synchronization variable, link it to the left-hand side.
+pub fn handle_aggregate_assignment<'tcx>(
+    place: &rustc_middle::mir::Place<'tcx>,
+    operands: &Vec<rustc_middle::mir::Operand<'tcx>>,
+    memory: &mut Memory<'tcx>,
+    caller_function_def_id: rustc_hir::def_id::DefId,
+    tcx: rustc_middle::ty::TyCtxt<'tcx>,
+) {
+    for operand in operands {
+        // Extract the place to be assignment
+        let rhs = match operand {
+            rustc_middle::mir::Operand::Copy(place) | rustc_middle::mir::Operand::Move(place) => {
+                place
+            }
+            // Nothing to do if we found a constant as one of the operands.
+            rustc_middle::mir::Operand::Constant(_) => continue,
+        };
+        handle_assignment_sync_variable(place, rhs, memory, caller_function_def_id, tcx);
+    }
+}
+
+/// Checks if the right-hand side contains a mutex, a lock guard, a join handle or a condition variable.
+/// If the right-hand side contains a synchronization variable, link it to the left-hand side.
+fn handle_assignment_sync_variable<'tcx>(
     place: &rustc_middle::mir::Place<'tcx>,
     rhs: &rustc_middle::mir::Place<'tcx>,
     memory: &mut Memory<'tcx>,
@@ -122,58 +131,5 @@ pub fn handle_ref_assignment<'tcx>(
         memory.link_place_to_same_join_handle(*place, *rhs);
     } else if is_place_with_concrete_type(rhs, "std::sync::Condvar", caller_function_def_id, tcx) {
         memory.link_place_to_same_condvar(*place, *rhs);
-    }
-}
-
-/// Handles MIR assignments of the form: `_X = { copy_data: move _Y }`.
-/// If the right-hand side contains a synchronization variable, link it to the left-hand side.
-pub fn handle_aggregate_assignment<'tcx>(
-    place: &rustc_middle::mir::Place<'tcx>,
-    operands: &Vec<rustc_middle::mir::Operand<'tcx>>,
-    memory: &mut Memory<'tcx>,
-    caller_function_def_id: rustc_hir::def_id::DefId,
-    tcx: rustc_middle::ty::TyCtxt<'tcx>,
-) {
-    for operand in operands {
-        // Extract the place to be assignment
-        let rhs = match operand {
-            rustc_middle::mir::Operand::Copy(place) | rustc_middle::mir::Operand::Move(place) => {
-                place
-            }
-            // Nothing to do if we found a constant as one of the operands.
-            rustc_middle::mir::Operand::Constant(_) => continue,
-        };
-
-        if is_place_with_concrete_type(rhs, "std::sync::Mutex<T>", caller_function_def_id, tcx)
-            || is_place_with_concrete_type(
-                rhs,
-                "std::sync::Arc<std::sync::Mutex<T>>",
-                caller_function_def_id,
-                tcx,
-            )
-        {
-            memory.link_place_to_same_mutex(*place, *rhs);
-        } else if is_place_with_concrete_type(
-            rhs,
-            "std::sync::MutexGuard<'a, T>",
-            caller_function_def_id,
-            tcx,
-        ) {
-            memory.link_place_to_same_lock_guard(*place, *rhs);
-        } else if is_place_with_concrete_type(
-            rhs,
-            "std::thread::JoinHandle<T>",
-            caller_function_def_id,
-            tcx,
-        ) {
-            memory.link_place_to_same_join_handle(*place, *rhs);
-        } else if is_place_with_concrete_type(
-            rhs,
-            "std::sync::Condvar",
-            caller_function_def_id,
-            tcx,
-        ) {
-            memory.link_place_to_same_condvar(*place, *rhs);
-        }
     }
 }
