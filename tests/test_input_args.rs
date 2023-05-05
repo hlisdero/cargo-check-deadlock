ca23d4558e415603e9d2db73c3f64ff5aed2dd1f
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-05 13:45:42 +0200

    Add a simple test to check that no file is generated by default

--- tests/test_input_args.rs
+++ tests/test_input_args.rs
@@ -33,7 +33,7 @@ fn output_folder_does_not_exist() {
 fn format_is_not_valid() {
     let file = assert_fs::NamedTempFile::new("valid_file.rs")
         .expect("Could not create temporary file for test");
-    file.write_str("fn main {}")
+    file.write_str("fn main() {}")
         .expect("Could not write test file contents");
 
     let mut cmd = Command::cargo_bin("granite2").expect("Command not found");
@@ -42,4 +42,30 @@ fn format_is_not_valid() {
     cmd.assert().failure().stderr(predicate::str::contains(
         "[possible values: pnml, lola, dot]",
     ));
+}
+
+#[test]
+fn does_not_generate_output_by_default() {
+    let file = assert_fs::NamedTempFile::new("valid_file.rs")
+        .expect("Could not create temporary file for test");
+    file.write_str("fn main() {}")
+        .expect("Could not write test file contents");
+
+    let mut cmd = Command::cargo_bin("granite2").expect("Command not found");
+
+    cmd.arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    if std::path::Path::new("./net.dot").exists() {
+        panic!("Should not generate a net.dot file by default");
+    }
+    if std::path::Path::new("./net.lola").exists() {
+        panic!("Should not generate a net.lola file by default");
+    }
+    if std::path::Path::new("./net.pnml").exists() {
+        panic!("Should not generate a net.pnml file by default");
+    }
 }
