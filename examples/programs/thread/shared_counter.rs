155c921ba683d630cc78c4295fb5f1a69ec7f20c
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-14 17:30:24 +0200

    Add test for threads where a shared counter is incremented

---
+++ examples/programs/thread/shared_counter.rs
@@ -0,0 +1,36 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    // Create a shared counter wrapped in a mutex
+    let counter = Arc::new(Mutex::new(0));
+
+    // Clone the shared counter for each thread
+    let counter1 = Arc::clone(&counter);
+    let counter2 = Arc::clone(&counter);
+
+    // Spawn the first thread
+    let thread1 = thread::spawn(move || {
+        // Increment the counter until it reaches 1000
+        while *counter1.lock().unwrap() < 1000 {
+            let mut num = counter1.lock().unwrap();
+            *num += 1;
+        }
+    });
+
+    // Spawn the second thread
+    let thread2 = thread::spawn(move || {
+        // Increment the counter until it reaches 1000
+        while *counter2.lock().unwrap() < 1000 {
+            let mut num = counter2.lock().unwrap();
+            *num += 1;
+        }
+    });
+
+    // Wait for both threads to finish
+    thread1.join().unwrap();
+    thread2.join().unwrap();
+
+    // Print the final value of the counter
+    println!("Final counter value: {}", *counter.lock().unwrap());
+}
--- tests/thread.rs
+++ tests/thread.rs
@@ -18,6 +18,15 @@ mod diverging {
     );
 }
 
+mod shared_counter {
+    use super::utils;
+
+    utils::generate_tests_for_example_program!(
+        "./examples/programs/thread/shared_counter.rs",
+        "./examples/results/thread/shared_counter/"
+    );
+}
+
 mod spawn_with_empty_closure {
     use super::utils;
 
