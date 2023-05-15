190610f7cb2df9fe02af3230f17a6ae79bfbdb70
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-15 13:33:42 +0200

    Add tests to check the result of the deadlock analysis for each program

---
+++ tests/lola.rs
@@ -0,0 +1,275 @@
+//! Tests for the model checker `LoLA`
+//!
+//! Check that the results matches the expected output for every program.
+
+mod utils;
+
+mod calculator {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/basic/calculator.rs",
+        "./examples/results/basic/calculator/",
+        false
+    );
+}
+
+mod greet {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/basic/greet.rs",
+        "./examples/results/basic/greet/",
+        false
+    );
+}
+
+mod hello_world {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/basic/hello_world.rs",
+        "./examples/results/basic/hello_world/",
+        false
+    );
+}
+
+mod infinite_wait_deadlock {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/condvar/infinite_wait_deadlock.rs",
+        "./examples/results/condvar/infinite_wait_deadlock/",
+        true
+    );
+}
+
+mod self_notify_lost_signal {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/condvar/self_notify_lost_signal.rs",
+        "./examples/results/condvar/self_notify_lost_signal/",
+        true
+    );
+}
+
+mod two_threads_sharing_condvar {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/condvar/two_threads_sharing_condvar.rs",
+        "./examples/results/condvar/two_threads_sharing_condvar/",
+        false
+    );
+}
+
+mod diverging {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/function_call/diverging.rs",
+        "./examples/results/function_call/diverging/",
+        true
+    );
+}
+
+mod find_even {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/function_call/find_even.rs",
+        "./examples/results/function_call/find_even/",
+        false
+    );
+}
+
+mod empty_function {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/function_call/empty_function.rs",
+        "./examples/results/function_call/empty_function/",
+        false
+    );
+}
+
+mod in_a_loop {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/function_call/in_a_loop.rs",
+        "./examples/results/function_call/in_a_loop/",
+        false
+    );
+}
+
+mod two_calls_same_function {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/function_call/two_calls_same_function.rs",
+        "./examples/results/function_call/two_calls_same_function/",
+        false
+    );
+}
+
+mod double_lock_deadlock_in_function {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/mutex/double_lock_deadlock_in_function.rs",
+        "./examples/results/mutex/double_lock_deadlock_in_function/",
+        true
+    );
+}
+
+mod double_lock_deadlock_with_arc {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/mutex/double_lock_deadlock_with_arc.rs",
+        "./examples/results/mutex/double_lock_deadlock_with_arc/",
+        true
+    );
+}
+
+mod double_lock_deadlock {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/mutex/double_lock_deadlock.rs",
+        "./examples/results/mutex/double_lock_deadlock/",
+        true
+    );
+}
+
+mod drop_mutex_guard_in_arc_manually {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/mutex/drop_mutex_guard_in_arc_manually.rs",
+        "./examples/results/mutex/drop_mutex_guard_in_arc_manually/",
+        false
+    );
+}
+
+mod drop_mutex_guard_manually {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/mutex/drop_mutex_guard_manually.rs",
+        "./examples/results/mutex/drop_mutex_guard_manually/",
+        false
+    );
+}
+
+mod two_threads_sharing_mutex {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/mutex/two_threads_sharing_mutex.rs",
+        "./examples/results/mutex/two_threads_sharing_mutex/",
+        false
+    );
+}
+
+mod abort {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/statement/abort.rs",
+        "./examples/results/statement/abort/",
+        true
+    );
+}
+
+mod empty_main {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/statement/empty_main.rs",
+        "./examples/results/statement/empty_main/",
+        false
+    );
+}
+
+mod infinite_loop {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/statement/infinite_loop.rs",
+        "./examples/results/statement/infinite_loop/",
+        false
+    );
+}
+
+mod match_stmt {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/statement/match.rs",
+        "./examples/results/statement/match/",
+        false
+    );
+}
+
+mod option {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/statement/option.rs",
+        "./examples/results/statement/option/",
+        false
+    );
+}
+
+mod panic {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/statement/panic.rs",
+        "./examples/results/statement/panic/",
+        false
+    );
+}
+
+mod detached {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/thread/detached.rs",
+        "./examples/results/thread/detached/",
+        false
+    );
+}
+
+mod diverging_thread {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/thread/diverging.rs",
+        "./examples/results/thread/diverging/",
+        false
+    );
+}
+
+mod shared_counter {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/thread/shared_counter.rs",
+        "./examples/results/thread/shared_counter/",
+        false
+    );
+}
+
+mod spawn_with_empty_closure {
+    use super::utils;
+
+    utils::generate_lola_tests_for_example_program!(
+        "./examples/programs/thread/spawn_with_empty_closure.rs",
+        "./examples/results/thread/spawn_with_empty_closure/",
+        false
+    );
+}
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
 /// Asserts that the contents of the given output file correspond to the expected file contents
@@ -7,8 +8,10 @@ use std::process::Command; // Run programs
 /// # Panics
 ///
 /// If the command `granite2` is not found, then the function panics.
+/// If the command `granite2` fails to complete execution sucessfully, then the function panics.
 /// If the output file cannot be opened, then the function panics.
 /// If the output file contents cannot be read, then the function panics.
+#[allow(dead_code)]
 pub fn assert_output_file(
     source_code_file: &str,
     output_folder: &str,
@@ -37,6 +40,42 @@ pub fn assert_output_file(
     std::fs::remove_file(output_filename).expect("Could not delete output file");
 }
 
+/// Asserts that the result of running the model checker  `LoLA` matches the expected result
+/// (program has a deadlock or deadlock-free) after running `granite2` on the given source code file.
+///
+/// # Panics
+///
+/// If the command `granite2` is not found, then the function panics.
+/// If the command `granite2` fails to complete execution sucessfully, then the function panics.
+#[allow(dead_code)]
+pub fn assert_lola_result(
+    source_code_file: &str,
+    output_folder: &str,
+    output_filename: &str,
+    output_should_have_deadlock: bool,
+) {
+    let mut cmd = Command::cargo_bin("granite2").expect("Command not found");
+
+    // Current workdir is always the project root folder
+    cmd.arg(source_code_file)
+        .arg(format!("--output-folder={output_folder}"))
+        .arg(format!("--format=lola"))
+        .arg("--filename=test")
+        .arg("--deadlock-analysis");
+
+    if output_should_have_deadlock {
+        cmd.assert().success().stdout(predicate::str::contains(
+            "Result: Deadlock can be reached according to the model checker `LoLA`",
+        ));
+    } else {
+        cmd.assert().success().stdout(predicate::str::contains(
+            "Result: The program is deadlock-free according to the model checker `LoLA`",
+        ));
+    }
+
+    std::fs::remove_file(output_filename).expect("Could not delete output file");
+}
+
 /// This macro generates the test code for the three supported file formats.
 /// It saves a considerable ammount of boilerplate.
 ///
@@ -47,6 +86,7 @@ pub fn assert_output_file(
 ///
 /// The main idea for the implementation was taken from:
 /// <https://doc.rust-lang.org/rust-by-example/macros/dry.html>
+#[allow(unused_macros)]
 macro_rules! generate_tests_for_example_program {
     ($program_path:literal, $result_folder_path:literal) => {
         #[test]
@@ -84,10 +124,38 @@ macro_rules! generate_tests_for_example_program {
     };
 }
 
-/// Exports the previously defined macro.
-/// For the idea for the re-export, see:
-/// <https://stackoverflow.com/questions/26731243/how-do-i-use-a-macro-across-module-files#31749071>
-/// A warning is generated if some test file does NOT use this function.
-/// That is because each test is compiled as an independent crate.
-/// See more details here: <https://stackoverflow.com/a/67902444>
+/// This macro generates the test code for the three supported file formats.
+/// It saves a considerable ammount of boilerplate.
+///
+/// Receives the relative path from the root folder of the repository
+/// to the source code of the program to be tested.
+/// Receives also the relative path from the root folder of the repository
+/// to the folder where the expected results are to be found.
+///
+/// The main idea for the implementation was taken from:
+/// <https://doc.rust-lang.org/rust-by-example/macros/dry.html>
+#[allow(unused_macros)]
+macro_rules! generate_lola_tests_for_example_program {
+    ($program_path:literal, $result_folder_path:literal, $expected_result:expr) => {
+        #[test]
+        fn generates_correct_lola_result() {
+            utils::assert_lola_result(
+                $program_path,
+                $result_folder_path,
+                concat!($result_folder_path, "test.lola"),
+                $expected_result,
+            );
+        }
+    };
+}
+
+// Exports the previously defined macro.
+// For the idea for the re-export, see:
+// <https://stackoverflow.com/questions/26731243/how-do-i-use-a-macro-across-module-files#31749071>
+// A warning is generated if some test file does NOT use this function.
+// That is because each test is compiled as an independent crate.
+// See more details here: <https://stackoverflow.com/a/67902444>
+#[allow(unused_imports)]
+pub(crate) use generate_lola_tests_for_example_program;
+#[allow(unused_imports)]
 pub(crate) use generate_tests_for_example_program;
