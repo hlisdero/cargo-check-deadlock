0950cd45d2a96aa3a1d909fc844c98eb03661b18
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-15 21:47:34 +0200

    Add a convenience script for printing the witness path

---
+++ scripts/run_lola_and_print_witness_path.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Simple script to run `lola` and print the witness path that shows HOW the deadlock is reached.
+# The witness path output is not capture by `granite2` and we need to resort to this workaround for now.
+lola --formula="EF (DEADLOCK AND (PROGRAM_END = 0 AND PROGRAM_PANIC = 0))" --path $1
