ba62955988bb784dece763596afee41734339116
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-15 21:15:16 +0200

    Add a convenience script for working with the test files

---
+++ scripts/accept_test_files_as_new_correct_result.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Simple script to accept the generated test results as the new correct result.
+# This saves a good amount of manual work when dealing with multiple failing tests.
+rename test net ./examples/results/**/**/test.*
