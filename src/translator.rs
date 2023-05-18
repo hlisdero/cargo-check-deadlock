222ae6c74163535300685ad4f38f0fdab4ed9001
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-18 22:19:32 +0200

    Move the contents of the file `function_call_handler.rs` to `translator.rs`

--- src/translator.rs
+++ src/translator.rs
@@ -26,22 +26,31 @@
 //! After every call the counter for the corresponding function is incremented.
 
 mod function;
-mod function_call_handler;
 mod mir_function;
 mod mir_visitor;
 mod special_function;
 mod sync;
 
 use crate::data_structures::hash_map_counter::HashMapCounter;
-use crate::data_structures::petri_net_interface::{PetriNet, PlaceRef, TransitionRef};
+use crate::data_structures::petri_net_interface::{
+    connect_places, PetriNet, PlaceRef, TransitionRef,
+};
 use crate::data_structures::stack::Stack;
+use crate::naming::function::{
+    foreign_call_transition_labels, indexed_mir_function_cleanup_label, indexed_mir_function_name,
+};
 use crate::naming::program::{PROGRAM_END, PROGRAM_PANIC, PROGRAM_START};
-use crate::utils::extract_def_id_of_called_function_from_operand;
-use function::Places;
+use crate::translator::function::{Places, Transitions};
+use crate::translator::mir_function::MirFunction;
+use crate::translator::special_function::{call_foreign_function, is_foreign_function};
+use crate::utils::{
+    extract_closure, extract_def_id_of_called_function_from_operand, extract_nth_argument_as_place,
+};
 use log::{debug, info};
-use mir_function::MirFunction;
 use rustc_middle::mir::visit::Visitor;
 use special_function::{call_diverging_function, call_panic_function, is_panic_function};
+use std::cell::RefCell;
+use std::rc::Rc;
 use sync::thread::Thread;
 
 /// Translator error message when no main function is found in the source code.
@@ -122,6 +131,38 @@ impl<'tcx> Translator<'tcx> {
         self.translate_top_call_stack();
         info!("Finished translating the main thread");
         self.translate_threads();
+    }
+
+    /// Main translation loop for the threads.
+    /// Iterate over the threads found and translate them.
+    /// If sync variables were passed to the thread, move them to the memory of the thread function.
+    /// Replaces the program panic place with the thread's end place
+    /// since abnormal thread termination does not affect the main thread.
+    fn translate_threads(&mut self) {
+        for thread in std::mem::take(&mut self.threads) {
+            let mut thread = thread.borrow_mut();
+            let index = thread.index;
+
+            info!("Starting translating thread {}", index);
+            let (thread_function_def_id, thread_start_place, thread_end_place) =
+                thread.prepare_for_translation(&mut self.net);
+            // Replace the panic place so that unwind transitions and similar point to the thread's end place.
+            self.program_panic = thread_end_place.clone();
+
+            self.push_function_to_call_stack(
+                thread_function_def_id,
+                thread_start_place,
+                thread_end_place,
+            );
+            info!("Pushed thread function to the translation call stack");
+
+            let new_function = self.call_stack.peek_mut();
+            info!("Moving sync variables to the thread function...");
+            thread.move_sync_variables(&mut new_function.memory, self.tcx);
+
+            self.translate_top_call_stack();
+            info!("Finished translating thread {}", index);
+        }
     }
 
     /// Pushes a new function frame to the call stack.
@@ -279,6 +320,272 @@ impl<'tcx> Translator<'tcx> {
         self.start_function_call(function_def_id, &function_name, args, destination, places);
     }
 
+    /// Starts the corresponding handler for the function call.
+    /// Checks if the function is one of the
+    /// supported synchronization or multithreading functions,
+    /// then if the function is a foreign function call and
+    /// lastly handle the standard MIR function case.
+    pub fn start_function_call(
+        &mut self,
+        function_def_id: rustc_hir::def_id::DefId,
+        function_name: &str,
+        args: &[rustc_middle::mir::Operand<'tcx>],
+        destination: rustc_middle::mir::Place<'tcx>,
+        places: Places,
+    ) {
+        // Special cases
+        if function_name == "std::mem::drop" {
+            self.call_mem_drop(function_name, args, destination, places);
+            self.function_counter.increment(function_name);
+            return;
+        }
+        if function_name == "std::result::Result::<T, E>::unwrap" {
+            self.call_result_unwrap(function_name, args, destination, places);
+            self.function_counter.increment(function_name);
+            return;
+        }
+        if function_name == "std::thread::spawn" {
+            self.call_thread_spawn(function_name, args, destination, places);
+            self.function_counter.increment(function_name);
+            return;
+        }
+        // Sync or multithreading function
+        if sync::is_supported_function(function_name) {
+            // Index for transition and place labels
+            let index = self.function_counter.get_count(function_name);
+            // A reference to the memory of the current function
+            let current_function = self.call_stack.peek_mut();
+            let memory = &mut current_function.memory;
+            // A reference to the Petri net to add transitions and places
+            let net = &mut self.net;
+
+            sync::call_supported_sync_function(
+                function_name,
+                index,
+                args,
+                destination,
+                places,
+                net,
+                memory,
+            );
+            self.function_counter.increment(function_name);
+            return;
+        }
+        // Default case for standard and core library calls
+        if is_foreign_function(function_def_id, function_name, self.tcx) {
+            self.call_foreign_function(function_name, args, destination, places);
+            self.function_counter.increment(function_name);
+            return;
+        }
+        // Default case: A function with MIR representation
+        self.call_mir_function(function_def_id, function_name, places);
+    }
+
+    /// Call to a MIR function. It is the default for user-defined functions in the code.
+    /// It is a recursive call for the translation process.
+    ///
+    /// A separate counter is incremented every time that
+    /// the function is called to generate a unique label.
+    fn call_mir_function(
+        &mut self,
+        function_def_id: rustc_hir::def_id::DefId,
+        function_name: &str,
+        places: Places,
+    ) {
+        let index = self.function_counter.get_count(function_name);
+        self.function_counter.increment(function_name);
+
+        match places {
+            Places::WithCleanup {
+                start_place,
+                end_place,
+                cleanup_place,
+            } => {
+                connect_places(
+                    &mut self.net,
+                    &start_place,
+                    &cleanup_place,
+                    &indexed_mir_function_cleanup_label(function_name, index),
+                );
+
+                self.call_stack.push(MirFunction::new(
+                    function_def_id,
+                    indexed_mir_function_name(function_name, index),
+                    start_place,
+                    end_place,
+                ));
+            }
+            Places::Basic {
+                start_place,
+                end_place,
+            } => {
+                self.call_stack.push(MirFunction::new(
+                    function_def_id,
+                    indexed_mir_function_name(function_name, index),
+                    start_place,
+                    end_place,
+                ));
+            }
+        }
+        info!("Pushed function {function_name} to the translation call stack");
+        self.translate_top_call_stack();
+    }
+
+    /// Call to a foreign function. It is the default for standard and core library calls.
+    /// It is a non-recursive call for the translation process.
+    /// It is also reused by other handlers since this is the basic case.
+    ///
+    /// Translates a call to a function with given function name using
+    /// the same representation as in `special_function::call_foreign_function`.
+    ///
+    /// A separate counter is incremented every time that
+    /// the function is called to generate a unique label.
+    ///
+    /// Performs a check to keep track of synchronization primitives.
+    /// In case the first argument is a mutex, mutex guard, join handle or condition variable,
+    /// it links the first argument of the function to its return value.
+    ///
+    /// Returns the transitions representing the function call.
+    fn call_foreign_function(
+        &mut self,
+        function_name: &str,
+        args: &[rustc_middle::mir::Operand<'tcx>],
+        destination: rustc_middle::mir::Place<'tcx>,
+        places: Places,
+    ) -> Transitions {
+        let index = self.function_counter.get_count(function_name);
+        let transitions = call_foreign_function(
+            places,
+            &foreign_call_transition_labels(function_name, index),
+            &mut self.net,
+        );
+
+        let current_function = self.call_stack.peek_mut();
+        sync::link_return_value_if_sync_variable(
+            args,
+            destination,
+            &mut current_function.memory,
+            current_function.def_id,
+            self.tcx,
+        );
+
+        transitions
+    }
+
+    /// Call to `std::mem::drop`.
+    /// Non-recursive call for the translation process.
+    fn call_mem_drop(
+        &mut self,
+        function_name: &str,
+        args: &[rustc_middle::mir::Operand<'tcx>],
+        destination: rustc_middle::mir::Place<'tcx>,
+        places: Places,
+    ) {
+        let transitions = self.call_foreign_function(function_name, args, destination, places);
+
+        let Some(dropped_place) = extract_nth_argument_as_place(args, 0) else {
+            panic!("BUG: `std::mem::drop` should receive the value to be dropped as a place");
+        };
+        match transitions {
+            Transitions::Basic { transition } => {
+                self.handle_mutex_guard_drop(dropped_place, &transition);
+            }
+            Transitions::WithCleanup {
+                transition,
+                cleanup_transition,
+            } => {
+                self.handle_mutex_guard_drop(dropped_place, &transition);
+                self.handle_mutex_guard_drop(dropped_place, &cleanup_transition);
+            }
+        }
+    }
+
+    /// Call to `std::result::Result::<T, E>::unwrap`.
+    /// Non-recursive call for the translation process.
+    ///
+    /// There is an important detail regarding how rustc interprets this call in conjunction with mutexes.
+    /// In some cases, the `std::result::Result::<T, E>::unwrap` function applied
+    /// to the return value of `std::sync::Mutex::<T>::lock` does not generate code to drop the mutex guard
+    /// since it considers that the mutex was never locked in the first place.
+    /// Nonetheless, for the purposes of the Petri net model, it is necessary to unlock the mutex in this case.
+    fn call_result_unwrap(
+        &mut self,
+        function_name: &str,
+        args: &[rustc_middle::mir::Operand<'tcx>],
+        destination: rustc_middle::mir::Place<'tcx>,
+        places: Places,
+    ) {
+        let transitions = self.call_foreign_function(function_name, args, destination, places);
+
+        if let Transitions::WithCleanup {
+            cleanup_transition, ..
+        } = transitions
+        {
+            let unwrapped_place = extract_nth_argument_as_place(args, 0)
+                .expect("BUG: `std::result::Result::<T, E>::unwrap` should receive the value to be unwrapped as a place");
+            self.handle_mutex_guard_drop(unwrapped_place, &cleanup_transition);
+        }
+    }
+
+    /// Call to `std::thread::spawn`.
+    /// Non-recursive call for the translation process.
+    ///
+    /// - Extracts the function `DefId` of the called function.
+    /// - Extracts the closure for the thread.
+    /// - Finds the sync variables passed in to the closure.
+    /// - Adds the thread to the `ThreadManager`.
+    /// - Links the return place to the `ThreadRef`.
+    fn call_thread_spawn(
+        &mut self,
+        function_name: &str,
+        args: &[rustc_middle::mir::Operand<'tcx>],
+        destination: rustc_middle::mir::Place<'tcx>,
+        places: Places,
+    ) {
+        let transitions = self.call_foreign_function(function_name, args, destination, places);
+        let transition = match transitions {
+            Transitions::Basic { transition } | Transitions::WithCleanup { transition, .. } => {
+                transition
+            }
+        };
+        // Extract the definition ID of the thread function
+        let current_function = self.call_stack.peek_mut();
+        let function_to_be_run = args
+            .get(0)
+            .expect("BUG: `std::thread::spawn` should receive the function to be run");
+        let thread_function_def_id = extract_def_id_of_called_function_from_operand(
+            function_to_be_run,
+            current_function.def_id,
+            self.tcx,
+        );
+
+        // Extract the closure and find the sync variables passed in to the closure
+        let closure_for_spawn = extract_closure(args);
+        let memory_entries =
+            sync::thread::find_sync_variables(closure_for_spawn, &mut current_function.memory);
+
+        // Add a new thread
+        let index = self.threads.len();
+        let thread = Rc::new(RefCell::new(sync::thread::Thread::new(
+            transition,
+            thread_function_def_id,
+            memory_entries.0,
+            memory_entries.1,
+            memory_entries.2,
+            memory_entries.3,
+            index,
+        )));
+        let thread_ref = Rc::clone(&thread);
+        self.threads.push(thread);
+        info!("Found thread {index} and pushed it to the back of the thread translation queue");
+
+        // The return value contains a new join handle. Link the local variable to it.
+        current_function
+            .memory
+            .link_place_to_join_handle(destination, &thread_ref);
+        debug!("NEW JOIN HANDLE: {destination:?}");
+    }
+
     /// Handler for the enum variant `TerminatorKind::Drop` in the MIR Visitor.
     /// <https://doc.rust-lang.org/stable/nightly-rustc/rustc_middle/mir/enum.TerminatorKind.html#variant.Drop>
     fn drop_terminator(
@@ -352,38 +659,6 @@ impl<'tcx> Translator<'tcx> {
                 function.assert(target, None, &mut self.net);
                 function.unreachable(&self.program_end, &mut self.net);
             }
-        }
-    }
-
-    /// Main translation loop for the threads.
-    /// Iterate over the threads found and translate them.
-    /// If sync variables were passed to the thread, move them to the memory of the thread function.
-    /// Replaces the program panic place with the thread's end place
-    /// since abnormal thread termination does not affect the main thread.
-    fn translate_threads(&mut self) {
-        for thread in std::mem::take(&mut self.threads) {
-            let mut thread = thread.borrow_mut();
-            let index = thread.index;
-
-            info!("Starting translating thread {}", index);
-            let (thread_function_def_id, thread_start_place, thread_end_place) =
-                thread.prepare_for_translation(&mut self.net);
-            // Replace the panic place so that unwind transitions and similar point to the thread's end place.
-            self.program_panic = thread_end_place.clone();
-
-            self.push_function_to_call_stack(
-                thread_function_def_id,
-                thread_start_place,
-                thread_end_place,
-            );
-            info!("Pushed thread function to the translation call stack");
-
-            let new_function = self.call_stack.peek_mut();
-            info!("Moving sync variables to the thread function...");
-            thread.move_sync_variables(&mut new_function.memory, self.tcx);
-
-            self.translate_top_call_stack();
-            info!("Finished translating thread {}", index);
         }
     }
 }
