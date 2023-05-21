622dbc0c5d7a0835b4c8fe403c84ca87fbdcde17
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-21 20:50:18 +0200

    Remove the version of the producers/consumers problem that uses only one condvar and keep the one that explicitly uses two condvars (which can be translated)

--- examples/programs/condvar/producer_consumer.rs
+++ examples/programs/condvar/producer_consumer.rs
@@ -1,44 +1,43 @@
+//! Explicitly use two separate `std::sync::Condvar` to be able to translate the program.
+//! Multiple calls to `wait` or `wait_while` are not supported yet.
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 
-/// This program implemented with only one `std::sync::Condvar`
-/// generates a Petri net that requires a considerable ammount of resources
-/// to be analyzed (> 16 GB RAM).
 fn main() {
-    let buffer = Arc::new((Mutex::new(0), Condvar::new()));
+    let buffer = Arc::new((Mutex::new(0), Condvar::new(), Condvar::new()));
 
     let producer_buffer = buffer.clone();
     let consumer_buffer = buffer.clone();
 
     let producer = thread::spawn(move || {
         for i in 1..10 {
-            let (lock, cvar) = &*producer_buffer;
+            let (lock, cvar_producer, cvar_consumer) = &*producer_buffer;
             let mut buffer = lock.lock().unwrap();
 
             while *buffer != 0 {
-                buffer = cvar.wait(buffer).unwrap();
+                buffer = cvar_producer.wait(buffer).unwrap();
             }
 
             *buffer = i;
             println!("Produced: {}", i);
 
-            cvar.notify_one();
+            cvar_consumer.notify_one();
         }
     });
 
     let consumer = thread::spawn(move || loop {
-        let (lock, cvar) = &*consumer_buffer;
+        let (lock, cvar_producer, cvar_consumer) = &*consumer_buffer;
         let mut buffer = lock.lock().unwrap();
 
         while *buffer == 0 {
-            buffer = cvar.wait(buffer).unwrap();
+            buffer = cvar_consumer.wait(buffer).unwrap();
         }
 
         let item = *buffer;
         *buffer = 0;
         println!("Consumed: {}", item);
 
-        cvar.notify_one();
+        cvar_producer.notify_one();
     });
 
     producer.join().unwrap();
