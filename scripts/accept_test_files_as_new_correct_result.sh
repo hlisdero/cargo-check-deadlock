4afb0f7cd097fde36ee955e12039fa4c643a45eb
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-23 17:28:52 +0200

    Rewrite script in a clearer way

--- scripts/accept_test_files_as_new_correct_result.sh
+++ scripts/accept_test_files_as_new_correct_result.sh
@@ -2,4 +2,5 @@
 
 # Simple script to accept the generated test results as the new correct result.
 # This saves a good amount of manual work when dealing with multiple failing tests.
+
 rename test net ./examples/results/**/**/test.*
