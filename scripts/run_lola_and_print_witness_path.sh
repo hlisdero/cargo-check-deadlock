5c2c456723d97376ad62b68d025cf50992686faa
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-23 17:13:14 +0200

    Fix typo

--- scripts/run_lola_and_print_witness_path.sh
+++ scripts/run_lola_and_print_witness_path.sh
@@ -6,7 +6,7 @@
 if ! command -v lola &>/dev/null; then
   echo "lola could not be found in \$PATH. Looking for the version included in the repository..."
   if [ -f .asserts/lola ]; then
-    echo "lola found in ./assets."
+    echo "lola found in ./assets"
     .asserts/lola --formula="EF (DEADLOCK AND (PROGRAM_END = 0 AND PROGRAM_PANIC = 0))" --path $1
   else
     echo "lola could not be found in ./assets"
