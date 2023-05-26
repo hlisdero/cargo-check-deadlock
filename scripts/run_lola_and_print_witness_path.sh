abec75baa86e9d23f604067f4038caa6a61bb1a7
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-26 18:06:33 +0200

    Add missing return in script

--- scripts/run_lola_and_print_witness_path.sh
+++ scripts/run_lola_and_print_witness_path.sh
@@ -6,6 +6,7 @@
 if command -v lola &>/dev/null; then
   echo "lola found in \$PATH."
   lola --formula="EF (DEADLOCK AND (PROGRAM_END = 0 AND PROGRAM_PANIC = 0))" --path $1
+  exit 0
 fi
 
 echo "lola could not be found in \$PATH. Looking for the version included in the repository..."
