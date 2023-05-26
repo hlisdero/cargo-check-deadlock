e50ca56828f92653a5b22ed41d51ba67a624aeee
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2023-05-26 18:26:04 +0200

    Add missing tests for `producer_consumer`

--- tests/condvar.rs
+++ tests/condvar.rs
@@ -7,6 +7,13 @@ mod infinite_wait_deadlock {
     );
 }
 
+mod producer_consumer {
+    super::utils::generate_tests_for_example_program!(
+        "./examples/programs/condvar/producer_consumer.rs",
+        "./examples/results/condvar/producer_consumer/"
+    );
+}
+
 mod self_notify_lost_signal {
     super::utils::generate_tests_for_example_program!(
         "./examples/programs/condvar/self_notify_lost_signal.rs",
