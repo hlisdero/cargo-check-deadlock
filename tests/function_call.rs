a525f9ba19a9f4b7d87f2d3e89316183742c3741
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2025-09-10 15:11:42 +0200

    test: example programs to test that argument passing works
    These should be similar to the existing programs to demostrate the easy
    cases at first

--- tests/function_call.rs
+++ tests/function_call.rs
@@ -8,6 +8,18 @@ mod function_call {
     );
 
     super::utils::generate_tests_for_example_program!(
+        double_lock_deadlock,
+        "./examples/programs/function_call/double_lock_deadlock.rs",
+        "./examples/results/function_call/double_lock_deadlock/"
+    );
+
+    super::utils::generate_tests_for_example_program!(
+        drop_mutex_guard_manually,
+        "./examples/programs/function_call/drop_mutex_guard_manually.rs",
+        "./examples/results/function_call/drop_mutex_guard_manually/"
+    );
+
+    super::utils::generate_tests_for_example_program!(
         find_even,
         "./examples/programs/function_call/find_even.rs",
         "./examples/results/function_call/find_even/"
@@ -26,6 +38,12 @@ mod function_call {
     );
 
     super::utils::generate_tests_for_example_program!(
+        self_notify_lost_signal,
+        "./examples/programs/function_call/self_notify_lost_signal.rs",
+        "./examples/results/function_call/self_notify_lost_signal/"
+    );
+
+    super::utils::generate_tests_for_example_program!(
         two_calls_same_function,
         "./examples/programs/function_call/two_calls_same_function.rs",
         "./examples/results/function_call/two_calls_same_function/"
@@ -38,6 +56,20 @@ mod lola {
         "./examples/programs/function_call/diverging.rs",
         "./examples/results/function_call/diverging/",
         true
+    );
+
+    super::utils::generate_lola_tests_for_example_program!(
+        double_lock_deadlock,
+        "./examples/programs/function_call/double_lock_deadlock.rs",
+        "./examples/results/function_call/double_lock_deadlock/",
+        true
+    );
+
+    super::utils::generate_lola_tests_for_example_program!(
+        drop_mutex_guard_manually,
+        "./examples/programs/function_call/drop_mutex_guard_manually.rs",
+        "./examples/results/function_call/drop_mutex_guard_manually/",
+        false
     );
 
     super::utils::generate_lola_tests_for_example_program!(
@@ -59,6 +91,13 @@ mod lola {
         "./examples/programs/function_call/in_a_loop.rs",
         "./examples/results/function_call/in_a_loop/",
         false
+    );
+
+    super::utils::generate_lola_tests_for_example_program!(
+        self_notify_lost_signal,
+        "./examples/programs/function_call/self_notify_lost_signal.rs",
+        "./examples/results/function_call/self_notify_lost_signal/",
+        true
     );
 
     super::utils::generate_lola_tests_for_example_program!(
